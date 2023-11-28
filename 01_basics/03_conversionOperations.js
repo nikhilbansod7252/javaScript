49f128ed5b42d8e1376d2130445b3f70aae2154c
Nikhil Bansod <nikhilb7252@gmail.com>
2023-11-28 20:09:46 +0000

    operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -21,7 +21,37 @@
 // "nikhil" => true
 
 
-let num = 33
-let stringNum = String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let num = 33
+// let stringNum = String(num)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//operations
+
+// let value = 3
+// let negativeVal = -value
+// console.log(negativeVal);
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2%2);
+
+// let str1 = "naruto"
+// let str2 = " uzumaki"
+// console.log(str1 + str2);
+
+// let counter = 100
+// ++counter;
+// console.log(counter);
+
+
+let x = 3;
+const y = x++;
+console.log(x, y);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+console.log(a, b);
+// Expected output: "a:4, b:4"
